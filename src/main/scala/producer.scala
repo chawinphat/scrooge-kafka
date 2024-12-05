960893112dcbf13db82021a2d4c9c65bd84a0fbf
Junseo Yoo <59751754+joonyoo181@users.noreply.github.com>
2024-12-04 17:50:14 -0800

    set acks to all so kafka runs consensus in between message exchanges

--- src/main/scala/producer.scala
+++ src/main/scala/producer.scala
@@ -49,6 +49,7 @@ object Producer {
     props.put("bootstrap.servers", brokerIps) // To test locally, change brokerIps with "localhost:9092"
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+    props.put("acks", "all")
 
     val producer = new KafkaProducer[String, Array[Byte]](props)
     if (configReader.shouldReadFromPipe()) { // Send message from Linux pipe
