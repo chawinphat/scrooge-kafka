76d7487e7b43aa6933d9d200432071c3bf4db06a
Junseo Yoo <joon.yoo181@berkeley.edu>
2024-12-05 17:31:56 -0800

    acks is now 1

--- src/main/scala/producer.scala
+++ src/main/scala/producer.scala
@@ -52,7 +52,7 @@ object Producer {
     props.put("bootstrap.servers", brokerIps) // To test locally, change brokerIps with "localhost:9092"
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
-    props.put("acks", "0")
+    props.put("acks", "1")
     props.put("linger.ms", 5)
     props.put("batch.size", 100000)
     props.put("buffer.memory", 1000000000)
