40aeeff0e899bde3347f6bca00b90dadad494a08
Junseo Yoo <joon.yoo181@berkeley.edu>
2024-12-05 17:23:22 -0800

    add batch size

--- src/main/scala/producer.scala
+++ src/main/scala/producer.scala
@@ -54,6 +54,7 @@ object Producer {
     props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
     props.put("acks", "0")
     props.put("linger.ms", 5)
+    props.put("batch.size", 100000)
     props.put("buffer.memory", 1000000000)
 
     val producer = new KafkaProducer[String, Array[Byte]](props)
