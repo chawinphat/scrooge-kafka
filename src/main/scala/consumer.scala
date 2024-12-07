df016f3fdac1d5a55c63109aaf307a24ec4d46d0
Junseo Yoo <joon.yoo181@berkeley.edu>
2024-12-06 16:50:47 -0800

    revert back to default properties for both consumer and producer

--- src/main/scala/consumer.scala
+++ src/main/scala/consumer.scala
@@ -71,9 +71,9 @@ object Consumer {
     props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer")
     props.put("auto.offset.reset", "latest")
     props.put("group.id", nodeId.toString)
-    props.put("fetch.min.bytes", 100000)
-    props.put("fetch.max.wait.ms", 5)
-    props.put("max.poll.records", 10000)
+    // props.put("fetch.min.bytes", 100000)
+    // props.put("fetch.max.wait.ms", 5)
+    // props.put("max.poll.records", 10000)
 
     val consumer: KafkaConsumer[String, Array[Byte]] = new KafkaConsumer[String, Array[Byte]](props)
     println("finished creating consumer")
