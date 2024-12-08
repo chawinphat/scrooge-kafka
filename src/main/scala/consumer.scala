708a3b8674be85272c076692b4e4e87c637b327e
Junseo Yoo <joon.yoo181@berkeley.edu>
2024-12-07 21:15:37 -0800

    Add consumer and producer tunes

--- src/main/scala/consumer.scala
+++ src/main/scala/consumer.scala
@@ -71,7 +71,7 @@ object Consumer {
     props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer")
     props.put("auto.offset.reset", "latest")
     props.put("group.id", nodeId.toString)
-    // props.put("fetch.min.bytes", 100000)
+    props.put("fetch.min.bytes", 100000)
 
     val consumer: KafkaConsumer[String, Array[Byte]] = new KafkaConsumer[String, Array[Byte]](props)
     println("finished creating consumer")
