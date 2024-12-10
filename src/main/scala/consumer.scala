29889d0335bb05db8de78e3bc719cbc17fc2c0c4
Reginald Frank <reginaldfrank77@berkeley.edu>
2024-12-10 05:16:17 -0800

    less extreme

--- src/main/scala/consumer.scala
+++ src/main/scala/consumer.scala
@@ -83,8 +83,8 @@ object Consumer {
     props.put("auto.offset.reset", "latest")
     props.put("group.id", System.currentTimeMillis().toString())  // last resort: props.put("group.id", None)
     props.put("fetch.max.wait.ms", "500")
-    props.put("fetch.min.bytes", "5000000")
-    props.put("max.partition.fetch.bytes", "5000000")
+    props.put("fetch.min.bytes", "10000000")
+    props.put("max.partition.fetch.bytes", "1000000")
     val numConsumers = 5
     val consumers = new ArrayList[KafkaConsumer[String, Array[Byte]]](numConsumers)
     var curConsumer = 0
